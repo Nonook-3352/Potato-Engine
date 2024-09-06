b2eb9ff4c3d510003a41e20ff20a761505de18ab
nonook <104591876+Nonook-3352@users.noreply.github.com>
2024-09-06 19:40:03 +0200

    fix: fixed drawSprite function trying to get sprite.texture.scale, but the scale was stored at sprite.scale

--- API/DrawAPI.lua
+++ API/DrawAPI.lua
@@ -1,10 +1,16 @@
 local DrawAPI = {
     drawSprite = function (sprite)
-        love.graphics.draw(sprite.texture.image, sprite.x, sprite.y, sprite.r, sprite.texture.scale[0], sprite.texture.scale[1])
+        love.graphics.draw(sprite.texture.image, sprite.x, sprite.y, sprite.r, sprite.scale[1], sprite.scale[2])
     end,
 
     drawText = function (text)
+        
+        if text.color[1] == nil then
+            print("text color must be an array with index (table)")
+            
+        else
         love.graphics.print({text.color, text.text}, text.x, text.y)
+        end
     end
 
 }
